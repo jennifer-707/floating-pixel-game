dd07ed26a01407ab152b29ddb6dd9ca819dee347
jennifer mercado <nticjennifer@gmail.com>
2026-06-14 21:24:51 -0700

    Se integraron todos los modulos

---
+++ main.py
@@ -0,0 +1,38 @@
+import pygame
+
+from config import *
+from assets import *
+from player import Player
+from pipes import *
+from game_logic import *
+
+pygame.init()
+
+# cargar recursos
+imagen_fondo = cargar_fondo()
+personajes = cargar_personajes()
+cargar_musica()
+
+# crear jugador
+player = Player()
+
+ejecucion = True
+
+while ejecucion:
+
+    for evento in pygame.event.get():
+        ...
+
+    player.actualizar()
+
+    tubos = mover_tubos(tubos)
+
+    if verificar_colision(
+        player.obtener_rect(),
+        tubos,
+        player.y,
+        city_height
+    ):
+        estado = GAMEOVER
+
+    pygame.display.flip()
